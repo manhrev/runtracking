9d7733da9024a2a0d0215b591ee90ed56a142a1c
manhrev <89514697+manhrev@users.noreply.github.com>
2022-12-04 15:38:46 +0700

    RT-9 add  authentication pages (#11)
    Co-authored-by: Manh Nguyen <manhnd@inspirelab.io>

---
+++ frontend/src/screens/Authentication/Intro.tsx
@@ -0,0 +1,92 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { ImageBackground, Platform, View } from "react-native";
+import { Button, Text } from "react-native-paper";
+import { RootBaseStackParamList } from "../../navigators/BaseStack";
+import { useAppTheme } from "../../theme";
+
+export default function Intro({
+  navigation,
+  route,
+}: NativeStackScreenProps<RootBaseStackParamList, "Intro">) {
+  const theme = useAppTheme();
+  return (
+    <View style={{ flex: 1, display: "flex" }}>
+      <View
+        style={{
+          flex: 3,
+          backgroundColor: "#FEFEFE",
+          justifyContent: "flex-end",
+          elevation: Platform.OS === "android" ? 0.1 : 0,
+          zIndex: 200,
+        }}
+      >
+        <View
+          style={{
+            position: "relative",
+            top: 50,
+          }}
+        >
+          <Text
+            variant="displaySmall"
+            style={{ textAlign: "center", fontWeight: "bold" }}
+          >
+            Welcome to{"\n"}Runtracking
+          </Text>
+          <Text
+            variant="titleLarge"
+            style={{ textAlign: "center", fontWeight: "bold" }}
+          >
+            Break your limits!
+          </Text>
+        </View>
+      </View>
+      <ImageBackground
+        style={{ flex: 10, justifyContent: "flex-end", zIndex: 2 }}
+        source={require("./../../../assets/Banners/intro2.jpg")}
+      >
+        <View
+          style={{
+            paddingHorizontal: 50,
+            position: "relative",
+            top: 30,
+            zIndex: 3,
+          }}
+        >
+          <Button
+            mode="contained"
+            onPress={() => {
+              navigation.navigate("Signup");
+            }}
+            style={{ marginTop: 30, borderRadius: 50 }}
+            contentStyle={{
+              borderRadius: 100,
+            }}
+            labelStyle={{
+              fontSize: 25,
+              paddingVertical: 10,
+              lineHeight: 30,
+              textAlign: "center",
+            }}
+          >
+            JOIN NOW!
+          </Button>
+        </View>
+      </ImageBackground>
+      <View style={{ flex: 4, backgroundColor: "#E8E8E8" }}>
+        <Text
+          variant="titleMedium"
+          style={{
+            fontWeight: "bold",
+            textAlign: "center",
+            color: theme.colors.primary,
+            position: "relative",
+            top: 30 + 10,
+          }}
+          onPress={() => navigation.navigate("Login")}
+        >
+          Already have an account?{"\n"}Login here!
+        </Text>
+      </View>
+    </View>
+  );
+}
--- frontend/src/screens/Profile/ProfileHome.tsx
+++ frontend/src/screens/Profile/ProfileHome.tsx
@@ -1,6 +1,6 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useState } from "react";
-import { StyleSheet, View, StatusBar } from "react-native";
+import { StyleSheet, View, StatusBar, ScrollView } from "react-native";
 import { Avatar, Button, Divider, IconButton, Text } from "react-native-paper";
 import { RootHomeTabsParamList } from "../../navigators/HomeTab";
 import { AppTheme, useAppTheme } from "../../theme";
@@ -25,82 +25,87 @@ export default function ProfileHome({
   };
   return (
     <>
-      <View style={styles(theme).profileBackgroundContainer}>
-        <Avatar.Text
-          size={170}
-          label="DT"
-          style={styles(theme).profilePicture}
-        />
-        <View style={styles(theme).profilePictureBack}></View>
-      </View>
-      <View style={styles(theme).extendedBaseContainer}>
-        <View style={baseStyles(theme).innerWrapper}>
-          <Text
-            variant="headlineMedium"
-            style={{ fontWeight: "bold", marginBottom: 15 }}
-          >
-            Duy Tuan
-          </Text>
-          <View style={{ display: "flex", flexDirection: "row" }}>
-            <View style={{ justifyContent: "center", flex: 1 }}>
-              <Button
-                icon="lead-pencil"
-                mode="contained-tonal"
-                onPress={handleEditYourProfile}
-              >
-                <Text variant="titleMedium" style={{ fontWeight: "bold" }}>
-                  Edit your profile
-                </Text>
-              </Button>
-            </View>
-            <View>
-              <IconButton
-                mode="contained-tonal"
-                icon="cog-outline"
-                onPress={handleSettingApp}
-              />
-            </View>
-            <View>
-              <IconButton
-                mode="contained-tonal"
-                icon="bell-outline"
-                onPress={handleViewNofification}
-                style={{ marginHorizontal: 0 }}
-              />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={{ marginTop: StatusBar.currentHeight }}
+      >
+        <View style={styles(theme).profileBackgroundContainer}>
+          <Avatar.Text
+            size={170}
+            label="DT"
+            style={styles(theme).profilePicture}
+          />
+          <View style={styles(theme).profilePictureBack}></View>
+        </View>
+        <View style={styles(theme).extendedBaseContainer}>
+          <View style={baseStyles(theme).innerWrapper}>
+            <Text
+              variant="headlineMedium"
+              style={{ fontWeight: "bold", marginBottom: 15 }}
+            >
+              Duy Tuan
+            </Text>
+            <View style={{ display: "flex", flexDirection: "row" }}>
+              <View style={{ justifyContent: "center", flex: 1 }}>
+                <Button
+                  icon="lead-pencil"
+                  mode="contained-tonal"
+                  onPress={handleEditYourProfile}
+                >
+                  <Text variant="titleMedium" style={{ fontWeight: "bold" }}>
+                    Edit your profile
+                  </Text>
+                </Button>
+              </View>
+              <View>
+                <IconButton
+                  mode="contained-tonal"
+                  icon="cog-outline"
+                  onPress={handleSettingApp}
+                />
+              </View>
+              <View>
+                <IconButton
+                  mode="contained-tonal"
+                  icon="bell-outline"
+                  onPress={handleViewNofification}
+                  style={{ marginHorizontal: 0 }}
+                />
+              </View>
             </View>
           </View>
         </View>
-      </View>
-      <Divider />
-      <View style={{ backgroundColor: theme.colors.background }}>
-        <View style={baseStyles(theme).innerWrapper}>
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              paddingVertical: 10,
-            }}
-          >
-            <Button
-              mode={isInfoSelected ? "elevated" : "text"}
-              onPress={() => {
-                setIsInfoSelected(true);
+        <Divider />
+        <View style={{ backgroundColor: theme.colors.background }}>
+          <View style={baseStyles(theme).innerWrapper}>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                paddingVertical: 10,
               }}
             >
-              Info
-            </Button>
-            <Button
-              mode={!isInfoSelected ? "elevated" : "text"}
-              style={{ marginLeft: 4 }}
-              onPress={() => setIsInfoSelected(false)}
-            >
-              Your achievements
-            </Button>
+              <Button
+                mode={isInfoSelected ? "elevated" : "text"}
+                onPress={() => {
+                  setIsInfoSelected(true);
+                }}
+              >
+                Info
+              </Button>
+              <Button
+                mode={!isInfoSelected ? "elevated" : "text"}
+                style={{ marginLeft: 4 }}
+                onPress={() => setIsInfoSelected(false)}
+              >
+                Your achievements
+              </Button>
+            </View>
           </View>
         </View>
-      </View>
-      <Divider />
-      <View>{isInfoSelected ? <ProfileInfo /> : <ProfileAchievement />}</View>
+        <Divider />
+        <View>{isInfoSelected ? <ProfileInfo /> : <ProfileAchievement />}</View>
+      </ScrollView>
     </>
   );
 }
@@ -117,7 +122,6 @@ const styles = (theme: AppTheme) =>
       height: 220,
       justifyContent: "flex-end",
       zIndex: 1,
-      marginTop: StatusBar.currentHeight,
     },
     profilePicture: {
       top: 38 + 175,
