4570406250a5b1f5fa239f6aff34acbac648a763
Manh Nguyen <manhnd@inspirelab.io>
2023-01-06 23:03:08 +0700

    RT-11 quick fix auth server

--- backend/auth/internal/server/auth/grpc_auth_me.go
+++ backend/auth/internal/server/auth/grpc_auth_me.go
@@ -3,7 +3,8 @@ package auth
 import (
 	"context"
 	"errors"
-	"log"
+
+	"github.com/manhrev/runtracking/backend/auth/pkg/ent/user"
 
 	"github.com/manhrev/runtracking/backend/auth/internal/status"
 	auth "github.com/manhrev/runtracking/backend/auth/pkg/api"
@@ -11,25 +12,49 @@ import (
 )
 
 func (s *authServer) Me(ctx context.Context, _ *emptypb.Empty) (*auth.MeReply, error) {
+	// userID, err := s.extractor.GetUserID(ctx)
+	// if err != nil {
+	// 	return nil, status.Unauthenticated
+	// }
+
+	// user, err := s.cache.User().Get(ctx, userID)
+	// if err != nil {
+	// 	log.Println("User not found")
+	// 	return nil, errors.New("User not found")
+	// }
+
+	// return &auth.MeReply{
+	// 	User: &auth.UserInfo{
+	// 		UserId:      user.UserId,
+	// 		Username:    user.UserName,
+	// 		Email:       user.Email,
+	// 		PhoneNumber: user.PhoneNumber,
+	// 		Weight:      user.Weight,
+	// 		Height:      user.Height,
+	// 	},
+	// }, nil
 	userID, err := s.extractor.GetUserID(ctx)
 	if err != nil {
 		return nil, status.Unauthenticated
 	}
 
-	user, err := s.cache.User().Get(ctx, userID)
+	user, err := s.entClient.User.Query().
+		Where(user.ID(userID)).
+		Only(ctx)
+
 	if err != nil {
-		log.Println("User not found")
 		return nil, errors.New("User not found")
 	}
 
 	return &auth.MeReply{
 		User: &auth.UserInfo{
-			UserId:      user.UserId,
-			Username:    user.UserName,
+			Username:    user.Username,
 			Email:       user.Email,
-			PhoneNumber: user.PhoneNumber,
-			Weight:      user.Weight,
+			PhoneNumber: user.Phone,
 			Height:      user.Height,
+			Weight:      user.Weight,
+			DisplayName: user.DisplayName,
+			Age:         int32(user.Height),
 		},
 	}, nil
 }
