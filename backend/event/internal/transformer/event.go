e243131ed91be082e535773135ab8b6f39b37057
Manh Nguyen <manhnd@inspirelab.io>
2023-05-02 14:56:38 +0700

    MANH-34 fix num of group in event

--- backend/event/internal/transformer/event.go
+++ backend/event/internal/transformer/event.go
@@ -18,7 +18,7 @@ func TransformEventEntToEvent(entEvent *ent.Event) *event_pb.EventDetail {
 		StartAt:      timestamppb.New(entEvent.StartAt),
 		EndAt:        timestamppb.New(endSubEvent.EndDate),
 		OwnerGroupId: entEvent.OwnerGroupID,
-		NumOfGroups:  entEvent.NumberOfGroups,
+		NumOfGroups:  0,
 	}
 }
 
@@ -27,12 +27,17 @@ func TransformEventEntListToEventList(entEvents []*ent.Event, yourGroupId int64)
 	for i, entEvent := range entEvents {
 		events[i] = TransformEventEntToEvent(entEvent)
 		yourGroupStatus := event_pb.GroupStatus_GROUP_STATUS_UNSPECIFIED
+		numOfGroups := 0
 		for _, participates := range entEvent.Edges.Participates {
+			if participates.Status == int64(event_pb.GroupStatus_GROUP_STATUS_ACTIVE) {
+				numOfGroups++
+			}
 			if participates.EventGroupID == yourGroupId {
 				yourGroupStatus = event_pb.GroupStatus(participates.Status)
 			}
 		}
 		events[i].YourGroupStatus = yourGroupStatus
+		events[i].NumOfGroups = uint32(numOfGroups)
 	}
 	return events
 }
