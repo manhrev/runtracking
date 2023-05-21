5053057e326bbea5f2ecdfbe674b90f86af77da1
Manh Nguyen <manhnd@ugaming.io>
2023-05-21 21:01:55 +0700

    MANH-39 add commit to event api

--- backend/activity/internal/server/activity/grpc_activity.go
+++ backend/activity/internal/server/activity/grpc_activity.go
@@ -4,15 +4,22 @@ import (
 	"github.com/manhrev/runtracking/backend/activity/internal/repository"
 	activity "github.com/manhrev/runtracking/backend/activity/pkg/api"
 	"github.com/manhrev/runtracking/backend/activity/pkg/ent"
+	event "github.com/manhrev/runtracking/backend/event/pkg/api"
 	group "github.com/manhrev/runtracking/backend/group/pkg/api"
 	plan "github.com/manhrev/runtracking/backend/plan/pkg/api"
 )
 
-func NewServer(entClient *ent.Client, planIClient plan.PlanIClient, groupIClient group.GroupIClient) activity.ActivityServer {
+func NewServer(
+	entClient *ent.Client,
+	planIClient plan.PlanIClient,
+	groupIClient group.GroupIClient,
+	eventIClient event.EventIClient,
+) activity.ActivityServer {
 	return &activityServer{
 		repository:   repository.New(entClient),
 		planIClient:  planIClient,
 		groupIClient: groupIClient,
+		eventIClient: eventIClient,
 	}
 }
 
@@ -20,6 +27,7 @@ type activityServer struct {
 	repository   *repository.Repository
 	planIClient  plan.PlanIClient
 	groupIClient group.GroupIClient
+	eventIClient event.EventIClient
 	// Other service client connection, db adapter go here
 	activity.UnimplementedActivityServer
 }
