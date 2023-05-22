234bdf7fad8ee64fc81b2af1374a8f78888ccfd9
ngoalong <phu.nguyen0808@hcmut.edu.vn>
2023-05-22 23:25:32 +0700

    [FE] Filter Fit Data by device chosen

--- frontend/src/utils/helpers/googlefit/index.ts
+++ frontend/src/utils/helpers/googlefit/index.ts
@@ -1,25 +1,45 @@
-import { AGGREGATES_BY, Point } from "../../../constants/googleapi";
+import { AGGREGATES_BY, Device, Point, PointValue } from "../../../constants/googleapi";
 import { GoogleFitRecordInfo } from "../../../screens/Profile/GoogleFitRecord";
 
-export const appendGoogleFitRecordValue = (type: string, points: Point[], googleFitRecord: GoogleFitRecordInfo) => {
+export const appendGoogleFitRecordValue = (type: string,
+     points: Point[],
+      googleFitRecord: GoogleFitRecordInfo, 
+      curDevice: Device,
+      deviceList: Device[]) => {
     points.forEach(point => {
-        if (point.value) {
-            point.value.forEach(pointValue => {
-                switch (type) {
-                    case AGGREGATES_BY.cal.dataSourceId:
-                        googleFitRecord.totalCalories += (pointValue.fpVal) ? pointValue.fpVal : 0
-                        break;
-                    case AGGREGATES_BY.dis.dataSourceId:
-                        googleFitRecord.totalDistance += (pointValue.fpVal) ? pointValue.fpVal : 0
-                        break;
-                    case AGGREGATES_BY.min.dataSourceId:
-                        googleFitRecord.totalTimeSpend += (pointValue.intVal) ? pointValue.intVal : 0
-                        break;
-                    default:
-                        break;
-                }
-            })
+        if(point.value){
+            if(curDevice.uid !== "Other" && point.originDataSourceId.includes(curDevice.manufacturer + ':' + curDevice.model + ':' + curDevice.uid)){
+             appendPointForGoogleFitRecord(type, point, googleFitRecord)
+        }else
+            if (curDevice.uid === "Other" && isOtherDevice(point.originDataSourceId, deviceList)) {
+             appendPointForGoogleFitRecord(type, point, googleFitRecord)
+            }
         }
     })
     return googleFitRecord
+}
+
+const appendPointForGoogleFitRecord = (type: string, point: Point, googleFitRecord: GoogleFitRecordInfo) =>{
+    point.value.forEach(pointValue => {
+                    switch (type) {
+                        case AGGREGATES_BY.cal.dataSourceId:
+                            googleFitRecord.totalCalories += (pointValue.fpVal) ? pointValue.fpVal : 0
+                            break;
+                        case AGGREGATES_BY.dis.dataSourceId:
+                            googleFitRecord.totalDistance += (pointValue.fpVal) ? pointValue.fpVal : 0
+                            break;
+                        case AGGREGATES_BY.min.dataSourceId:
+                            googleFitRecord.totalTimeSpend += (pointValue.intVal) ? pointValue.intVal : 0
+                            break;
+                        default:
+                            break;
+                    }
+                })
+}
+
+const isOtherDevice = (originDataSourceId: string, deviceList: Device[]) => {
+    for(let curDevice of deviceList)
+      if(originDataSourceId.includes(curDevice.manufacturer + ':' + curDevice.model + ':' + curDevice.uid))
+            return false
+    return true
 }
